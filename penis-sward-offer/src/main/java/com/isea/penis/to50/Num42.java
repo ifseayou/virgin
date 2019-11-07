0161099df638f8af53aa78c7c1ea619c254fa96b
谢海龙 <ifseayou@gmail.com>
2019-11-07 20:33:46 +0800

    求有序数组中两个数等于固定值，输出ArrayList

--- penis-sward-offer/src/main/java/com/isea/penis/to50/Num42.java
+++ penis-sward-offer/src/main/java/com/isea/penis/to50/Num42.java
@@ -12,13 +12,44 @@ import java.util.ArrayList;
 public class Num42 {
     public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
         ArrayList<Integer> res = new ArrayList<>();
-        return res;
+        int i = array.length - 1;
+        int j = array.length - 1;
+        int left = 0;
+        int right = array.length - 1;
+        while(left < right){
+            int help = array[left] + array[right];
+            if (help == sum){
+                if (help < array[i] * array[j]){
+                    i = left;
+                    j = right;
+                }
+                right --;
+            }else if (help < sum){
+                left ++;
+            }else {
+                right --;
+            }
+        }
+        if (i == j){
+            return res;
+        }else {
+            res.add(array[i]);
+            res.add(array[j]);
+            return res;
+        }
     }
     public static void main(String[] args) {
         Num42 num42 = new Num42();
+
+        int[] arr2 = {1,2,4,7,11,16};
+        ArrayList<Integer> res2 = num42.FindNumbersWithSum(arr2, 10);
+        if (res2 != null){
+            res2.forEach(t-> System.out.print(t + " "));
+        }
+
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
         ArrayList<Integer> res = num42.FindNumbersWithSum(arr, 10);
-
+        res.forEach(t-> System.out.print(t + " "));
 
     }
 }
