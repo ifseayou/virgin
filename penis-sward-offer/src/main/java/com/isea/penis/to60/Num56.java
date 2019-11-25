61946b6886abb0e8a05e8477b82a84246e1a668a
谢海龙 <ifseayou@gmail.com>
2019-11-25 21:27:26 +0800

    求一个链表的入环节点，无环返回null

--- penis-sward-offer/src/main/java/com/isea/penis/to60/Num56.java
+++ penis-sward-offer/src/main/java/com/isea/penis/to60/Num56.java
@@ -1,26 +1,56 @@
 package com.isea.penis.to60;
+import java.util.HashSet;
 
 /**
  * @author isea_you
  * @date 2019/11/22
  * @time 10:43
- * @target:  给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ * @target: 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
  */
 public class Num56 {
-    public static class ListNode{
+    public static class ListNode {
         private int val;
         private ListNode next;
-        public ListNode(int val){
+
+        public ListNode(int val) {
             this.val = val;
         }
     }
 
     public ListNode EntryNodeOfLoop(ListNode pHead) {
-            return pHead;
+        if (pHead == null) {
+            return null;
+        }
+        HashSet help = new HashSet<ListNode>();
+        ListNode cur = pHead;
+        while (cur != null) {
+            if (help.contains(cur)) {
+                return cur;
+            }
+            help.add(cur);
+            cur = cur.next;
+        }
+        return null;
     }
+
+
     public static void main(String[] args) {
         Num56 num56 = new Num56();
 
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);
+        System.out.println(num56.EntryNodeOfLoop(head1));
+
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(4);
+        head2.next.next.next.next = new ListNode(5);
+        head2.next.next.next.next.next = head2.next.next;
+        System.out.println(num56.EntryNodeOfLoop(head2).val);
     }
 }
 
