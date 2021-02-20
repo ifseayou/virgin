420de8ccecf6dd2f23580ea73997ca5634b580f7
谢海龙 <ifseayou@gmail.com>
2021-02-20 21:13:33 +0800

    traverseBinaryTree

--- clitoris-algo/src/main/java/com/isea/clitoris/tree/traverse/NoRecTraverse.java
+++ clitoris-algo/src/main/java/com/isea/clitoris/tree/traverse/NoRecTraverse.java
@@ -6,7 +6,7 @@ import java.util.Stack;
  * 二叉树的非递归先序遍历
  */
 public class NoRecTraverse {
-    public class Node {
+    public static class Node {
         private int data;
         private Node left;
         private Node right;
@@ -17,66 +17,72 @@ public class NoRecTraverse {
         }
     }
 
-    // 先序遍历=> 接助辅助栈实现
+    /**
+     * 先序遍历=> 借助辅助栈实现
+     */
     public static void preOrder(Node head){
-        System.out.println("Pre-Order-NoRec");
-        Stack<Node> help = new Stack<>();
-        if (head == null){
-            return;
-        }
-        help.push(head);
-        while(!help.isEmpty()){
-             head = help.pop();
-            System.out.println(head.data + " ");
-            if (head.right != null){
-                help.push(head.right);
-            }
-            if (head.left != null){
-                help.push(head.left);
-            }
-        }
-        System.out.println();
-    }
-
-    // 中序遍历,左根右，如果当前节点为null，从栈中取出一个元素打印，当前节点往右；如果当前节点不为null，当前节点入栈，当前节点向左
-    public static void inOrder(Node head){
-        System.out.println("In-Order-NoRec");
-        if (head == null){
-            return;
-        }
-        Stack<Node> help = new Stack<>();
-        while(!help.isEmpty() || head != null){
-            if (head != null){
-                help.push(head);
-                head = head.left;
-            }else {
-                head = help.pop();
-                System.out.println(head.data + " ");
-                head = head.right;
-            }
-        }
-        System.out.println();
-    }
-
-    // 后续遍历，左右根 ： 先实现根右左，打印的时候不打印，而是放入到栈中
-    public static void postOrder(Node head){
-        System.out.println("Post-Order-NoRec");
+        System.out.println("Pre-Order:");
         if (head != null){
             Stack<Node> stack = new Stack<>();
-            Stack<Node> help = new Stack<>();
             stack.push(head);
             while(!stack.isEmpty()){
                 head = stack.pop();
-                help.push(head);
-                if (head.left != null){
-                    stack.push(head.left);
-                }
+                System.out.print(head.data);
                 if (head.right != null){
                     stack.push(head.right);
                 }
+                if (head.left != null){
+                    stack.push(head.left);
+                }
             }
-            while(!help.isEmpty()){
-                System.out.print(help.pop().data + " ");
+        }
+        System.out.println();
+    }
+
+
+    /**
+     *中序遍历,左根右，如果当前节点为null，从栈中取出一个元素打印，当前节点往右；如果当前节点不为null，当前节点入栈，当前节点向左
+     */
+    public static void inOrder(Node head){
+        System.out.println("In-Order:");
+        if (head != null){
+            Stack<Node> stack = new Stack<>();
+            while(!stack.isEmpty() || head != null){
+                if (head != null){
+                    stack.push(head);
+                    head = head.left;
+                }else {
+                    head = stack.pop();
+                    System.out.print(head.data);
+                    head = head.right;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+
+    /**
+     * 后续遍历，左右根 ： 先实现根右左，打印的时候不打印，而是放入到栈中
+     */
+    public static void postOrder(Node head){
+        System.out.println("Post-Order:");
+        if (head != null){
+            Stack<Node> stack1 = new Stack<>();
+            Stack<Node> stack2 = new Stack<>();
+            stack1.push(head);
+            while(!stack1.isEmpty()){
+                head = stack1.pop();
+                stack2.push(head);
+                if (head.left != null){
+                    stack1.push(head.left);
+                }
+                if (head.right != null){
+                    stack1.push(head.right);
+                }
+            }
+            while(!stack2.isEmpty()){
+                System.out.print(stack2.pop().data);
             }
         }
         System.out.println();
--- clitoris-algo/src/main/java/com/isea/clitoris/tree/traverse/TraverseBinaryTree.java
+++ clitoris-algo/src/main/java/com/isea/clitoris/tree/traverse/TraverseBinaryTree.java
@@ -4,7 +4,7 @@ package com.isea.clitoris.tree.traverse;
  * 遍历二叉树，前序，中序，后序的递归写法
  */
 public class TraverseBinaryTree {
-    public  class Node{
+    public static class Node{
         private int data;
         private Node left;
         private Node right;
@@ -16,18 +16,21 @@ public class TraverseBinaryTree {
     }
 
     public static void preOrder(Node node){
+        if (node == null) return;
         System.out.print(node.data + "->");
         preOrder(node.left);
         preOrder(node.right);
     }
 
     public static void inOrder(Node node){
+        if (node == null) return;
         inOrder(node.left);
         System.out.print(node.data + "->");
         inOrder(node.right);
     }
 
     public static void postOrder(Node node){
+        if (node == null) return;
         postOrder(node.left);
         postOrder(node.right);
         System.out.print(node.data + "->");
